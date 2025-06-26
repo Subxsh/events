b0168b7b47e1a3f3711f8978cd2c3cbe270b0d26
Subxsh <subashperiyasamy25@gmail.com>
2025-06-26 15:49:47 +0530

    eve

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
 
   const fetchEvents = async () => {
     try {
-      const response = await axios.get('/api/events');
+      const response = await axios.get('https://eventsdepploy.onrender.com/api/events');
       setEvents(response.data);
       console.log('Fetched events:', response.data);
     } catch (error) {
