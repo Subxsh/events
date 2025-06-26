32e6a6caf5983d932c8fc318abbe86cf6c562f5c
Subxsh <155135026+Subxsh@users.noreply.github.com>
2025-06-26 15:36:53 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
 
   const fetchEvents = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/events');
+      const response = await axios.get('https://eventsdepploy.onrender.com/api/events');
       setEvents(response.data);
       
     } catch (error) {
